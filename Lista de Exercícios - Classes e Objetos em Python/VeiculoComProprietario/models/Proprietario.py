1c174b3f2a444ff9e45ad41980594b5904be4448
IagoDiotti <iagodiotti@gmail.com>
2025-06-02 08:34:10 -0300

    ADDED: VeiculoComProprietario

---
+++ Lista de Exercícios - Classes e Objetos em Python/VeiculoComProprietario/models/Proprietario.py
@@ -0,0 +1,29 @@
+import re
+
+class Proprietario:
+    def __init__(self, nome, cpf):
+        if not self.validar_cpf(cpf):
+            raise ValueError("CPF inválido")
+        self.nome = nome
+        self.cpf = cpf
+        self.veiculos = []  # Lista de placas de veículos
+
+    def validar_cpf(self, cpf):
+        cpf = re.sub(r'[^0-9]', '', cpf)
+        return len(cpf) == 11
+
+    def validar_placa(self, placa):
+        padrao = r'^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$'
+        return re.match(padrao, placa) is not None
+
+    def adicionar_veiculo(self, placa):
+        if self.validar_placa(placa):
+            if placa not in self.veiculos:
+                self.veiculos.append(placa)
+            else:
+                raise ValueError("Veículo já cadastrado para este proprietário.")
+        else:
+            raise ValueError("Placa inválida")
+
+    def __str__(self):
+        return f'{self.nome} (CPF: {self.cpf}) - Veículos: {", ".join(self.veiculos) if self.veiculos else "Nenhum"}'
